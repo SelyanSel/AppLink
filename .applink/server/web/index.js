05d52ac59f44b0cf0c2ea839cd2cca459c09f81c
Alazard Célian <celian.alazard@gmail.com>
2023-08-05 13:28:05 +0200

    1.0.1
    This updates only adds the automatic update checker.

--- .applink/server/web/index.js
+++ .applink/server/web/index.js
@@ -5,3 +5,17 @@ function github(){
 function openLink(link){
     window.location = link
 }
+
+var updateIndicator = document.getElementById('updateNeeded');
+var updateIndicatorFalse = document.getElementById('updateNotNeeded');
+
+fetch('https://raw.githubusercontent.com/SelyanSel/AppLink/main/updater/version.ref', {})
+    .then(response => response.text())
+    .then(text => {
+        if (text === "1.0.1" || text === "1.0.1\n"){
+            updateIndicator.remove();
+        }else{
+            updateIndicatorFalse.remove();
+        }
+    }
+)
